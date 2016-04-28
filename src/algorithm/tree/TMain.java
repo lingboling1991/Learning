5186435f6f1852175c19f161cc4d6ae178db8be0
lingboling1991 <lingboling1991@126.com>
2016-04-28 18:13:03 +0800

    BST 2 Linked List

--- src/algorithm/tree/TMain.java
+++ src/algorithm/tree/TMain.java
@@ -7,22 +7,34 @@ public class TMain {
 		TreeNode a = new TreeNode(1);
 		TreeNode b = new TreeNode(2);
 		TreeNode c = new TreeNode(3);
-		TreeNode d = new TreeNode(-4);
+		TreeNode d = new TreeNode(4);
 		TreeNode e = new TreeNode(5);
+		TreeNode f = new TreeNode(6);
+		TreeNode g = new TreeNode(7);
 
 		a.left = b;
 		a.right = c;
 		b.left = d;
 		b.right = e;
+		c.left = f;
+		c.right = g;
 
+		// 二叉排序树转换为双向链表
+		Convert2Linkedlist.traversal(a);
+		Convert2Linkedlist.printHead();
+
+		// 树中价值最大的路径，https://leetcode.com/problems/binary-tree-maximum-path-sum/
 		int maxPathSum = MaxPathSum.maxPathSum(a);
 		TreeNode upsideDown = UpsideDownBT.UpsideDownBinaryTree(a);
 
+		// 二叉树最大最小深度
 		int max = MaxMinDepth.maxDepth(a);
 		int min = MaxMinDepth.minDepthII(a);
 
+		// 是否是平衡二叉树
 		boolean res = ValidateBalanced.isValid(a);
 
+		// 数组转化成二叉排序树
 		int[] n = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 		TreeNode root = Convert2BST.fromSortedArray(n);
 
