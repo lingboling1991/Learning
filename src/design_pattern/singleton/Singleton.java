9e159dc40467788a407ad28005e9d59eaf21e512
lingboling1991 <lingboling1991@126.com>
2016-05-13 10:01:10 +0800

    add bucket_sort and chatroom

--- src/design_pattern/singleton/Singleton.java
+++ src/design_pattern/singleton/Singleton.java
@@ -4,9 +4,8 @@ public class Singleton {
 
 	private static Singleton INSTANCE = null;
 
-	private Singleton() {// 私有构造函数，避免new出新实例
-
-	}
+	private Singleton() {
+	}// 私有构造函数，避免new出新实例
 
 	public static Singleton getInstance() {// 双重同步锁
 		if (INSTANCE == null) {
